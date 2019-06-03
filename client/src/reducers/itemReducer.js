9ce0769d0c00846a836f82abb2b7e92231f98b7a
jjadeseravla <jadealvares@hotmail.co.uk>
2019-06-03 15:58:41 +0100

    (WIP) finishing redux

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -17,6 +17,11 @@ export default function(state = initialState, action) {
       return {
         ...state //return whatever is in items array
       };
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter(item => item.id !== action.payload)
+      }
     default:
       return state;
   }
--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,7 @@
 //entry point to redux store
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from '../reducers/';
+import rootReducer from './reducers';
 
 const initialState = {};
 
